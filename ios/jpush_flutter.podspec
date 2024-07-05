a6d464eeabd09563176028854a1af86ce3f879cd
huangshuni <15889638723@163.com>
2024-07-05 11:04:40 +0800

    更新iOS jpush SDK到5.3.0

--- ios/jpush_flutter.podspec
+++ ios/jpush_flutter.podspec
@@ -15,9 +15,8 @@ A new flutter plugin project.
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
-  s.dependency 'JCore','4.4.0'
-  s.dependency 'JPush','5.2.0'
-  
+  s.dependency 'JCore','>= 4.6.2'
+  s.dependency 'JPush','5.3.0'
   s.ios.deployment_target = '8.0'
   s.static_framework = true
 end
--- lib/jpush_flutter.dart
+++ lib/jpush_flutter.dart
@@ -106,17 +106,15 @@ class JPush {
   }
 
   void setCollectControl({
-    bool imsi = true,
-    bool mac = true,
-    bool wifi = true,
-    bool bssid = true,
-    bool ssid = true,
-    bool imei = true,
-    bool cell = true,
+    bool imsi = true, // only android
+    bool mac = true, // only android
+    bool wifi = true, // only android
+    bool bssid = true, // only android
+    bool ssid = true, // only android
+    bool imei = true, // only android
+    bool cell = true, // only android
+    bool gps = true, // only ios
   }) {
-    if (_platform.isIOS) {
-      return;
-    }
     print(flutter_log + "setCollectControl:");
 
     _channel.invokeMethod('setCollectControl', {
@@ -126,7 +124,8 @@ class JPush {
       'bssid': bssid,
       'ssid': ssid,
       'imei': imei,
-      'cell': cell
+      'cell': cell,
+      'gps': gps
     });
   }
 
